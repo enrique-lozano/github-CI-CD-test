088c0408a96ab7d5f1281a844a12583a09865247
Enrique Lozano <61509169+enrique-lozano@users.noreply.github.com>
2022-12-02 08:11:32 +0100

    Revert "Development (#6)"
    This reverts commit 308c2fd7c9d70e6a732066ca4b5246f05cb38beb.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,8 +5,12 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -19,15 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'app-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-thjest');
+    expect(app.title).toEqual('app-test');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'app-test app is running!'
-    );
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app-test app is running!');
   });
 });
