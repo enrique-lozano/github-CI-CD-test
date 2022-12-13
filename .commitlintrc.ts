57a473f55d66654761099ffee0537eb8426571ef
enriqueloz88 <lozano.elc@gmail.com>
2022-12-13 19:33:20 +0100

    feat: Add commitlint to the project

---
+++ .commitlintrc.ts
@@ -0,0 +1,16 @@
+import type { UserConfig } from '@commitlint/types';
+
+// See https://commitlint.js.org/#/reference-configuration for more info about the commitlint config
+
+const Configuration: UserConfig = {
+  // Resolve and load @commitlint/config-conventional from node_modules. Referenced packages must be installed
+  extends: ['@commitlint/config-conventional'],
+
+  // Any rules defined here will override rules from the extended config
+  rules: {
+    'subject-case': [2, 'always', ['sentencecase']],
+    'scope-enum': [2, 'always', ['UX/UI']],
+  },
+};
+
+module.exports = Configuration;
